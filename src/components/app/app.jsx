12765149b0dea6c9037516b15692a61d50f03e8f
Fadeev Ivan <githubfadeev@gmail.com>
2019-09-18 09:56:27 +0300

    Created function for deleting item in todos list

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import ItemStatusFilter from '../item-status-filter';
 import TodoList from '../todo-list';
 
-const App = () => {
-  const appData = [
-    {label: "Make breakfast", important: false, id: 1},
-    {label: "Create React App", important: true, id: 2},
-    {label: "Make something", important: false, id: 3}
-  ]; 
 
-  return(
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <ItemStatusFilter/>
-        <SearchPanel/>
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      appData: [
+        {label: "Make breakfast", important: false, id: 1},
+        {label: "Create React App", important: true, id: 2},
+        {label: "Make something", important: false, id: 3}
+      ] 
+    };
+
+    this.deleteItem = (id) => {
+      this.setState( ({appData})=> {
+        const idx = appData.findIndex( (el) => el.id === id);
+        //Для того, чтобы не изменять state напрямую,
+        //используем метод slice, вместо splice
+        const newAppData = [
+          ...appData.slice(0, idx),
+          ...appData.slice(idx+1)
+        ]
+        return {
+          appData: newAppData
+        }
+      })
+    };
+  }
+
+  render() {
+    return(
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <ItemStatusFilter/>
+          <SearchPanel/>
+        </div>
+        <TodoList todos = {this.state.appData} onDeleted = {this.deleteItem}/>
       </div>
-      <TodoList todos = {appData}/>
-    </div>
-  )
-} 
-
-export default App;
+    )
+  }
+}
